84aaed07f63e78f38ac727624395104e521b348b
Brockston King <brockston.king@gmail.com>
2019-08-30 14:50:16 -0600

    Updates

---
+++ precache-manifest.8812a9dab41811fa8884410ef5bae249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3253dbf1a76c154d10372b8e1f2bd3",
+    "url": "/portfolioreact/index.html"
+  },
+  {
+    "revision": "474ed9ce887290b04015",
+    "url": "/portfolioreact/static/css/main.2077c3bc.chunk.css"
+  },
+  {
+    "revision": "912483dba220ffbe7f3a",
+    "url": "/portfolioreact/static/js/2.221479b9.chunk.js"
+  },
+  {
+    "revision": "474ed9ce887290b04015",
+    "url": "/portfolioreact/static/js/main.d8fce0c8.chunk.js"
+  },
+  {
+    "revision": "d29aaf3a396893a69f39",
+    "url": "/portfolioreact/static/js/runtime~main.a3c73f76.js"
+  }
+]);
